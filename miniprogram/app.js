8d136ad68cdc99d7aa29655d64109770106b0419
Qucheng Miao <qcmiao@hotmail.com>
2020-02-20 14:50:49 +0800

    1450

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 "use strict";
 App({
     globalData: {},
-    onLaunch: function () {
+    onLaunch() {
     },
 });
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFFQSxHQUFHLENBQWE7SUFDZCxVQUFVLEVBQUUsRUFBRTtJQUNkLFFBQVE7SUFFUixDQUFDO0NBQ0YsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gYXBwLnRzXHJcblxyXG5BcHA8SUFwcE9wdGlvbj4oe1xyXG4gIGdsb2JhbERhdGE6IHt9LFxyXG4gIG9uTGF1bmNoKCkge1xyXG4gICAgXHJcbiAgfSxcclxufSkiXX0=
