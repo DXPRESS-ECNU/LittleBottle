0e7937cbe5b6acbfd69406d0b1178bcca1071dc9
smally <2907047490@qq.com>
2020-02-20 20:31:04 +0800

    1112

--- miniprogram/pages/index/export.ts
+++ miniprogram/pages/index/export.ts
@@ -19,48 +19,90 @@ export const Export=(bottles: any[],success: Function,needauth: Function,getauth
   const drawAll = (context: WechatMiniprogram.CanvasContext, bottleUrl: string): void =>{
     const bottleHeight = 300
     const bottleWidth = 200
-    const titleHeight = 100
+    const titleHeight = 280
     const picWidth = 100
-
-    //画背景
-    context.rect(0, 0, 1300, 4000)
-    context.setFillStyle("white")
-    context.fill()
-    //context.draw()
-    context.setFillStyle("black")
     
-    let bottleIndex = 0
-    for(let y = 1; y <= 6; ++y){
-      for (let x = 1; x <= 5; ++x) {
-        for (let i = 1; i <= bottles[bottleIndex].full; ++i){
-          switch(i){
-            case 1:{
-              drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 195, picWidth, picWidth])
-              break
+    const bgUrl = "cloud://dxzsbottle-iu8cx.6478-dxzsbottle-iu8cx-1301327315/background.jpg"
+    wx.cloud.downloadFile({
+      fileID: bgUrl,
+      success: res =>{
+        context.drawImage(res.tempFilePath ,0, 0, 1300, 2800)
+        let bottleIndex = 0
+        for (let y = 1; y <= 6; ++y) {
+          for (let x = 1; x <= 5; ++x) {
+            for (let i = 1; i <= bottles[bottleIndex].full; ++i) {
+              switch (i) {
+                case 1: {
+                  drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 195, picWidth, picWidth])
+                  break
+                }
+                case 2: {
+                  drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 150, picWidth, picWidth])
+                  break
+                }
+                case 3: {
+                  drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 95, picWidth, picWidth])
+                  break
+                }
+                case 4: {
+                  drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 50, picWidth, picWidth])
+                  break
+                }
+                case 5: {
+                  drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight - 5, picWidth, picWidth])
+                  break
+                }
+              }
             }
-            case 2:{
-              drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 150, picWidth, picWidth])
-              break
-            }
-            case 3:{
-              drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 95, picWidth, picWidth])
-              break
-            }
-            case 4:{
-              drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 50, picWidth, picWidth])
-              break
-            }
-            case 5: {
-              drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight-5, picWidth, picWidth])
-              break
-            }
+            //画瓶子
+            drawPic(context, bottleUrl, [50 * x + bottleWidth * (x - 1), (bottleHeight + 100) * (y - 1) + titleHeight, bottleWidth, bottleHeight])
+            drawText(context, bottles[bottleIndex].name, [50 * x + bottleWidth * (x - 1) + 40, (bottleHeight + 50) * y + 50 * (y - 1) + titleHeight, 150])
+            bottleIndex++
           }
         }
-        //画瓶子
-        drawPic(context, bottleUrl, [50 * x + bottleWidth * (x - 1), (bottleHeight + 100) * (y - 1) + titleHeight, bottleWidth, bottleHeight])
-        drawText(context, bottles[bottleIndex].name, [50*x+bottleWidth*(x-1)+40, (bottleHeight+50)*y+50*(y-1)+titleHeight, 150])
-        bottleIndex++
-      }
+      },
+      fail: console.error
+    })
+
+    // //画背景
+    // context.rect(0, 0, 1300, 4000)
+    // context.setFillStyle("white")
+    // context.fill()
+    // //context.draw()
+    // context.setFillStyle("black")
+    
+    // let bottleIndex = 0
+    // for(let y = 1; y <= 6; ++y){
+    //   for (let x = 1; x <= 5; ++x) {
+    //     for (let i = 1; i <= bottles[bottleIndex].full; ++i){
+    //       switch(i){
+    //         case 1:{
+    //           drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 195, picWidth, picWidth])
+    //           break
+    //         }
+    //         case 2:{
+    //           drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 150, picWidth, picWidth])
+    //           break
+    //         }
+    //         case 3:{
+    //           drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight + 95, picWidth, picWidth])
+    //           break
+    //         }
+    //         case 4:{
+    //           drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 5, (bottleHeight + 100) * (y - 1) + titleHeight + 50, picWidth, picWidth])
+    //           break
+    //         }
+    //         case 5: {
+    //           drawPic(context, bottles[bottleIndex].path, [50 * x + bottleWidth * (x - 1) + 85, (bottleHeight + 100) * (y - 1) + titleHeight-5, picWidth, picWidth])
+    //           break
+    //         }
+    //       }
+    //     }
+    //     //画瓶子
+    //     drawPic(context, bottleUrl, [50 * x + bottleWidth * (x - 1), (bottleHeight + 100) * (y - 1) + titleHeight, bottleWidth, bottleHeight])
+    //     drawText(context, bottles[bottleIndex].name, [50*x+bottleWidth*(x-1)+40, (bottleHeight+50)*y+50*(y-1)+titleHeight, 150])
+    //     bottleIndex++
+    //  }
     }
   }
 
